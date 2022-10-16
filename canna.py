11aacd84a61b41c2d13e63cd2364283110a2d544
a1 <mmvedyashkin@edu.hse.ru>
2022-10-16 12:13:16 -0700

    удалил ненужные файлы

--- canna.py
+++
@@ -1,3 +0,0 @@
-
-"""test_file = ["nummbers.txt", "nummbers_2.txt", "nummbers_3.txt", "nummbers_4.txt", "nummbers_5.txt", "nummbers_6.txt",
-             "nummbers_7.txt", "nummbers_8.txt", "nummbers_9.txt", "nummbers_10.txt"]"""
